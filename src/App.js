cce0418305d34852d1bc73997df16ec164cdd0be
labibahmed10 <labib.ahmed.372@gmail.com>
2022-03-25 16:05:52 +0600

    using react modal

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./Navbar/Navbar";
 import Card from "./Card/Card";
 import { useEffect, useState } from "react";
+import Modal from "react-modal/lib/components/Modal";
+
+// Modal style
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+//saving to root
+Modal.setAppElement("#yourAppElement");
 
 function App() {
   const [guns, setGuns] = useState([]);
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     fetch("data.json")
       .then((res) => res.json())
       .then((data) => setGuns(data));
   }, []);
+
+  const addToCart = (product) => {
+    const newCart = [...cart, product];
+    setCart(newCart);
+    console.log(newCart);
+  };
+
   return (
     <div className="App">
       {/* navbar */}
@@ -21,7 +44,7 @@ function App() {
 
       <section className="gun-container">
         {guns.map((gun) => (
-          <Card key={gun.id} gun={gun}></Card>
+          <Card key={gun.id} gun={gun} addToCart={addToCart}></Card>
         ))}
       </section>
     </div>
--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,20 +1,24 @@
 import React from "react";
 import "./Card.css";
+import { BsFillCartCheckFill } from "react-icons/bs";
 
-const Card = ({ gun }) => {
-  console.log(gun);
+const Card = ({ gun, addToCart }) => {
   const { img, action, bullet, name, price, capacity } = gun;
   return (
     <div className="card">
       <div className="image-container">
         <img src={img} alt="" />
       </div>
-      <h1>{name}</h1>
-      <p>Bullet-type: {bullet}</p>
-      <p>Capacity: {capacity}</p>
-      <p>Action: {action}</p>
+      <div className="gun-info">
+        <h1>{name}</h1>
+        <p>Bullet-type: {bullet}</p>
+        <p>Capacity: {capacity}</p>
+        <p>Action: {action}</p>
+      </div>
       <div className="add-to-cart">
-        <button>Add to Cart</button>
+        <button onClick={() => addToCart(gun)}>
+          <BsFillCartCheckFill className="icon"></BsFillCartCheckFill>
+        </button>
         <h1>Price: {price}</h1>
       </div>
     </div>
